bc74619a89ac78501a28f2a190d9b5b1985c2a09
cpedrohenrique1 <88724009+cpedrohenrique1@users.noreply.github.com>
2022-12-01 18:42:48 -0300

    update com funcoes

--- comfuncoes.cpp
+++ comfuncoes.cpp
@@ -2,29 +2,71 @@
 using namespace std;
 #include <stdio.h>
 
-void abrirArquivo(FILE *arquivo, char modo){
+FILE *abrirArquivo(char modo){
+    FILE *arquivo;
     switch (modo){
-        case 'g':
+        case 'w':
             arquivo = fopen("test.txt", "w");
+            cout << "Arquivo criado\n";
             break;
-        case 'l':
+        case 'r':
             arquivo = fopen("test.txt", "r");
+            cout << "Arquivo aberto\n";
+            break;
+        case 'a':
+            arquivo = fopen("test.txt", "a");
+            break;
+        default:
+            throw string("Modo invalido, tente novamente\n");
             break;
     }
 
     if (arquivo == NULL){
-        throw string("Arquivo não encontrado");
+        throw string("Arquivo nao encontrado");
     }
+    return arquivo;
 }
 
 void fecharArquivo(FILE *arquivo){
     fclose (arquivo);
 }
+
+void editarArquivo( char entrada[30]){
+    FILE *arquivo;
+    arquivo = abrirArquivo('a');
+    fprintf(arquivo, "%s\n", entrada);
+}
  
 int main(){
     //      Abertura de Arquivos     //
     FILE *arquivo;
-    
+    char modo;
+    char entrada[30];
+    try{
+        cout << "Insira o modo de arquivo: ";
+        cin >> modo;
+        arquivo = abrirArquivo(modo);
+        switch (modo){
+            case 'w':
+                cout << "Insira o texto: ";
+                cin >> entrada;
+                editarArquivo(entrada);
+                break;
+            case 'r':
+                while (fscanf(arquivo, "%s", entrada) != EOF){
+                    cout << entrada << endl;
+                }
+                break;
+            case 'a':
+                cout << "Insira o texto: ";
+                cin >> entrada;
+                editarArquivo(entrada);
+                break;
+        }
+    }
+    catch(string error){
+        cout << "Erro " << error << endl;
+    }
 
     return 0;
 }
